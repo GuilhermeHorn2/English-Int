bea50d1165ea24c0471927d60279dec65b8271a2
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-06-26 20:21:23 -0300

    Create tests.java

---
+++ tests.java
@@ -0,0 +1,22 @@
+package misc;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+
+		English_int eng = new English_int();
+		System.out.println(eng.get_word(999));
+		System.out.println(eng.get_word(127131));
+		
+	
+		
+		
+}
+	
+
+	
+	
+}	
